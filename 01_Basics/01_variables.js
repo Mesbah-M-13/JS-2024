64729cb3983862ea1ac59c7f348276df0bce45e0
Mesbah-M-13 <139665223+Mesbah-M-13@users.noreply.github.com>
2024-02-22 20:06:26 +0000

    variable in js

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1212
+let accountEmail ="himel@gmail.com"
+var accountPassword="12345"
+accountCity = "Dhaka"
+let accountState;
+
+// accountId = 202 // not allowed , TypeError: Assignment to constant variable.
+// console.log(accountId);
+
+accountEmail ="himel@fb.com"
+accountPassword="0076"
+accountCity ="Khulna"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
