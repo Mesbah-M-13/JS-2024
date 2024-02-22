5fa43b961f98bddb8e7782f546b220d8bde45762
Mesbah-M-13 <139665223+Mesbah-M-13@users.noreply.github.com>
2024-02-22 20:21:46 +0000

    variable concept updated

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -2,7 +2,7 @@ const accountId = 1212
 let accountEmail ="himel@gmail.com"
 var accountPassword="12345"
 accountCity = "Dhaka"
-let accountState;
+let accountState;  // declaring variable without any assigned value
 
 // accountId = 202 // not allowed , TypeError: Assignment to constant variable.
 // console.log(accountId);
