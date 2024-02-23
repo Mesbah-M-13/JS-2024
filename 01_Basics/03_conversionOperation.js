7861a8c2f6b55dd0f8e0b1e81505575fa105e5c9
Mesbah-M-13 <139665223+Mesbah-M-13@users.noreply.github.com>
2024-02-23 06:04:54 +0000

    added conversion operation

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "44"
+
+// 2 ways to write typeof
+ console.log(typeof(score));
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+
+// let score1 = "44abc" // string with number and word 
+// console.log(typeof score1);
+// let valueInNumber1 = Number(score1)
+// console.log(valueInNumber1); // NaN
+
+// let score2 = null // null to number conversion
+// console.log(typeof score2);
+// let valueInNumber2 = Number(score2)
+// console.log(typeof valueInNumber2); 
+// console.log(valueInNumber2); // 0
+
+let score3 = undefined // undefined to number conversion
+console.log(typeof score3);
+let valueInNumber3 = Number(score3)
+console.log(typeof valueInNumber3); 
+console.log(valueInNumber3); // 0
