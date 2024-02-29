1e74a1b0cacfcce693a0d03c1b839de016462d30
Mesbah-M-13 <139665223+Mesbah-M-13@users.noreply.github.com>
2024-02-29 15:29:33 +0000

    little changes are done

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -6,30 +6,31 @@ const repoCount = 50
 //console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('himel-hm-com')
+//console.log(gameName.length);
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
 
 
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
-//console.log(gameName.charAt(2));
-//console.log(gameName.indexOf('t'));
+// console.log(gameName.length);                //1. length
+// console.log(gameName.toUpperCase());         //2. toUpperCase
+//console.log(gameName.charAt(2));              //3. charAt
+console.log(gameName.indexOf('l'));             //4. indexOf
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
+const newString = gameName.substring(0, 3)      //5. substring ( -ve not allowed,if put - it changes to 0)
+//console.log(newString);
 
-const anotherString = gameName.slice(-8, 4)
-//console.log(anotherString);
+const anotherString = gameName.slice(-12, 5)     //6. slice
+console.log(anotherString);
 
 const newStringOne = "   himel    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// console.log(newStringOne);
+// console.log(newStringOne.trim());            //7. trim
 
 const url = "https://himel.com/himel%20mesbah"
 
-console.log(url.replace('%20', '-'))
+console.log(url.replace('%20', '-'))            //8. replace
 
-console.log(url.includes('bah'))
+console.log(url.includes('bah'))                //9. includes
 
-console.log(gameName.split('-'));
+console.log(gameName.split('-'));               //10. split
